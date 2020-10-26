5c6e18cd306e8403bd369a4ca240350c4af86ae2
Daviking3110 <70233189+Daviking3110@users.noreply.github.com>
2020-10-26 13:28:47 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : Ex2_2
+Auteur(s) : Anthony David
+Date creation :
+Description : <à compléter>
+Remarque(s) : <à compléter>
+Compilateur : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+*/
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+<code>
+return EXIT_SUCCESS;
+}
